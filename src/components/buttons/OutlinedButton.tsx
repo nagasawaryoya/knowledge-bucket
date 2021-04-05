15546a15581659050d73403a6d2a1daa136bd274
nagasawaryoya <nagasawaryoya@gmail.com>
2021-04-05 23:56:38 +0900

    枠線ボタンコンポーネント内でデフォルトのスタイルを設定

---
+++ src/components/buttons/OutlinedButton.tsx
@@ -0,0 +1,72 @@
+import React, { FC } from 'react';
+import MuiButton from '@material-ui/core/Button';
+import { ButtonProps as MuiButtonProps } from '@material-ui/core';
+import { makeStyles, Theme, createStyles, useTheme } from '@material-ui/core/styles';
+import { THEME_TYPE } from 'unions/ui-theme/theme-type';
+import { COLOR } from 'unions/ui-theme/color';
+import { BORDER, BUTTON, CommonStyles } from 'unions/ui-theme/style';
+
+type ButtonStyles = Omit<React.CSSProperties, 'color' | 'backgroundColor' | 'borderColor'> & {
+  width?: CommonStyles.ButtonWidth;
+  height?: CommonStyles.ButtonHeight;
+  borderRadius?: CommonStyles.BorderRadius;
+  borderWidth?: CommonStyles.BorderWidth;
+};
+
+type ButtonProps = MuiButtonProps & {
+  label: string;
+  button?: Omit<MuiButtonProps, 'variant' | 'color'>;
+  style?: ButtonStyles;
+};
+
+/**
+ * 外枠ボタンコンポーネント。
+ */
+export const OutlinedButton: FC<ButtonProps> = ({ label, button, style }) => {
+  const classes = useStyles(style);
+  return (
+    <MuiButton variant="outlined" {...button} className={classes.root}>
+      {label}
+    </MuiButton>
+  );
+};
+
+/**
+ * スタイルを適用する。
+ */
+const useStyles = (style?: ButtonStyles) => {
+  const currenTheme = useTheme();
+
+  return makeStyles((theme: Theme) =>
+    createStyles({
+      root: {
+        width: style?.width ?? BUTTON.WIDTH.M,
+        height: style?.height ?? BUTTON.HEIGHT.M,
+        borderColor: COLOR.GREY.MAIN,
+        borderWidth: style?.borderWidth ?? BORDER.WIDTH.M,
+        borderRadius: style?.borderRadius ?? BORDER.RADIUS.S,
+        borderStyle: style?.borderStyle ?? 'solid',
+        ...styling(currenTheme),
+      },
+    }),
+  )();
+};
+
+/**
+ * テーマに応じたスタイルを返却する。
+ */
+const styling = (theme: Theme) =>
+  theme.palette.type === THEME_TYPE.DARK
+    ? {
+        color: COLOR.GREY.LIGHT,
+        '&:hover': {
+          backgroundColor: COLOR.NAVY.MAIN,
+        },
+      }
+    : {
+        color: COLOR.BLACK.DARK,
+        '&:hover': {
+          color: COLOR.WHITE.MAIN,
+          backgroundColor: COLOR.BLUE.MAIN,
+        },
+      };
--- src/pages/app/App.tsx
+++ src/pages/app/App.tsx
@@ -1,34 +1,26 @@
 import React from 'react';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import { IconButton } from 'components/buttons/IconButton';
-import { Button } from 'components/buttons/Button';
+import { ContainedButton } from 'components/buttons/ContainedButton';
 import { Dialog } from 'components/layouts/Dialog';
 import { InputText } from 'components/inputs/InputText';
 import { COLOR } from 'unions/ui-theme/color';
-import { BORDER, BUTTON } from 'unions/ui-theme/style';
 import { ICON_NAME } from 'unions/icon-name';
 import { THEME_TYPE } from 'unions/ui-theme/theme-type';
 import { createTheme } from 'consts/theme';
+import { OutlinedButton } from 'components/buttons/OutlinedButton';
 import './App.css';
 
 function App() {
   return (
-    <ThemeProvider theme={createTheme(THEME_TYPE.DARK)}>
+    <ThemeProvider theme={createTheme(THEME_TYPE.LIGHT)}>
       <CssBaseline />
       <React.Fragment>
         <IconButton icon={{ name: ICON_NAME.ADD }} />
-        <Button label="作成" />
-        <Button
-          label="作成"
-          style={{
-            width: BUTTON.WIDTH.L,
-            backgroundColor: COLOR.GREEN.DARK,
-            borderRadius: BORDER.RADIUS.C,
-            borderColor: COLOR.RED.DARK,
-            borderWidth: BORDER.WIDTH.M,
-            borderStyle: 'solid',
-          }}
-        />
+        <IconButton icon={{ name: ICON_NAME.ADD }} style={{ hoverColor: COLOR.GREEN.MAIN }} />
+        <ContainedButton label="作成" style={{ backgroundColor: COLOR.GREEN.MAIN }} />
+        <ContainedButton label="作成" />
+        <OutlinedButton label="キャンセル" />
         <Dialog open={false} title="タイトルですよ" text="テキストテキスト" />
         <InputText></InputText>
       </React.Fragment>
