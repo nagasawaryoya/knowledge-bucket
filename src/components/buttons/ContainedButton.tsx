ba00536ebcf0bbb3662a32b43d775974f28d6199
nagasawaryoya <nagasawaryoya@gmail.com>
2021-04-05 23:55:57 +0900

    塗り潰しボタンコンポーネント内でデフォルトのスタイルを設定

---
+++ src/components/buttons/ContainedButton.tsx
@@ -0,0 +1,74 @@
+import React, { FC } from 'react';
+import MuiButton from '@material-ui/core/Button';
+import { ButtonProps as MuiButtonProps } from '@material-ui/core';
+import { makeStyles, Theme, createStyles, useTheme } from '@material-ui/core/styles';
+import { THEME_TYPE } from 'unions/ui-theme/theme-type';
+import { COLOR, Color } from 'unions/ui-theme/color';
+import { BORDER, BUTTON, CommonStyles } from 'unions/ui-theme/style';
+
+type ButtonStyles = Omit<React.CSSProperties, 'color' | 'borderColor'> & {
+  backgroundColor?: Color;
+  width?: CommonStyles.ButtonWidth;
+  height?: CommonStyles.ButtonHeight;
+  borderRadius?: CommonStyles.BorderRadius;
+  borderWidth?: CommonStyles.BorderWidth;
+};
+
+type ButtonProps = MuiButtonProps & {
+  label: string;
+  button?: Omit<MuiButtonProps, 'variant'>;
+  style?: ButtonStyles;
+};
+
+/**
+ * 塗り潰しボタンコンポーネント。
+ */
+export const ContainedButton: FC<ButtonProps> = ({ label, button, style }) => {
+  const classes = useStyles(style);
+  return (
+    <MuiButton variant="contained" {...button} className={classes.root}>
+      {label}
+    </MuiButton>
+  );
+};
+
+/**
+ * スタイルを適用する。
+ */
+const useStyles = (style?: ButtonStyles) => {
+  const currenTheme = useTheme();
+
+  return makeStyles((theme: Theme) =>
+    createStyles({
+      root: {
+        width: style?.width ?? BUTTON.WIDTH.M,
+        height: style?.height ?? BUTTON.HEIGHT.M,
+        backgroundColor: style?.backgroundColor,
+        borderWidth: style?.borderWidth ?? BORDER.WIDTH.M,
+        borderRadius: style?.borderRadius ?? BORDER.RADIUS.S,
+        borderStyle: style?.borderStyle ?? 'solid',
+        ...styling(currenTheme, style),
+      },
+    }),
+  )();
+};
+
+/**
+ * テーマに応じたスタイルを返却する。
+ */
+const styling = (theme: Theme, style?: ButtonStyles) =>
+  theme.palette.type === THEME_TYPE.DARK
+    ? {
+        borderColor: style?.backgroundColor ?? COLOR.NAVY.MAIN,
+        '&:hover': {
+          color: style?.backgroundColor ?? COLOR.NAVY.MAIN,
+          backgroundColor: COLOR.BLACK.MAIN,
+        },
+      }
+    : {
+        borderColor: style?.backgroundColor ?? COLOR.BLUE.MAIN,
+        '&:hover': {
+          color: style?.backgroundColor ?? COLOR.BLUE.MAIN,
+          backgroundColor: COLOR.WHITE.MAIN,
+        },
+      };
