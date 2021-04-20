faab69ff777222879ee5404660768b9f334d75ca
nagasawaryoya <nagasawaryoya@gmail.com>
2021-04-20 12:31:08 +0900

    ページ層をContainerコンポーネントでラップする

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import { CssBaseline, ThemeProvider } from '@material-ui/core';
-import { THEME_TYPE } from 'unions/ui-theme/theme-type';
-import { createTheme } from 'consts/theme';
+import { Container } from '@material-ui/core';
+import { ARIA_LABEL } from 'unions/test/aria-label';
 
 const Analytics = () => {
   return (
-    <ThemeProvider theme={createTheme(THEME_TYPE.DARK)}>
-      <CssBaseline />
-      <React.Fragment>
-        <h1>analytics</h1>
-      </React.Fragment>
-    </ThemeProvider>
+    <Container aria-label={ARIA_LABEL.ANALYTICS}>
+      <h1>analytics</h1>
+    </Container>
   );
 };
 
