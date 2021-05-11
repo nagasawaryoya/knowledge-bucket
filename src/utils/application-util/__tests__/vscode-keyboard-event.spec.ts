75c8fd2128e9dc4b47d8de3bc15a1a2e03d45bcb
nagasawaryoya <nagasawaryoya@gmail.com>
2021-05-12 08:18:52 +0900

    VscodeKeyboardEventクラスのテストファイル追加

---
+++ src/utils/application-util/__tests__/vscode-keyboard-event.spec.ts
@@ -0,0 +1,24 @@
+// import VscodeKeyboardEvent, { KeyEvent } from '../vscode-keyboard-event';
+
+// const createInstance = (e: KeyEvent) => new VscodeKeyboardEvent(e);
+
+describe('VscodeKeyboardEventクラスのテスト', () => {
+  it('デフォルトで設定されたタブのサイズが 4 であることをテストする', () => {
+    // const instance = createInstance();
+    // expect(instance.TAB_SIZE).toBe(4);
+  });
+  describe('イベントテスト', () => {
+    it('エンター押下', () => {
+      // createInstance();
+    });
+    it('コマンド+エンター押下', () => {
+      // createInstance();
+    });
+    it('タブ押下', () => {
+      // createInstance();
+    });
+    it('シフト+タブ押下', () => {
+      // createInstance();
+    });
+  });
+});
