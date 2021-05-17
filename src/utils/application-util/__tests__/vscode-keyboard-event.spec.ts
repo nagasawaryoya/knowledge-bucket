47580ba96a5c704926d966fdb4bbb5e74826139a
nagasawaryoya <nagasawaryoya@gmail.com>
2021-05-18 08:43:21 +0900

    VscodeKeyboardEvent クラスのテスト準備

--- src/utils/application-util/__tests__/vscode-keyboard-event.spec.ts
+++ src/utils/application-util/__tests__/vscode-keyboard-event.spec.ts
@@ -1,23 +1,90 @@
-// import VscodeKeyboardEvent, { KeyEvent } from '../vscode-keyboard-event';
+import VscodeKeyboardEvent, { Target } from '../vscode-keyboard-event';
 
-// const createInstance = (e: KeyEvent) => new VscodeKeyboardEvent(e);
+const createInstance = (t: Target) => new VscodeKeyboardEvent(t);
+
+const NORMAL_TEXT = `
+# Hello, World!
+## My name is JavaScript
+`.trim();
+
+const PAD = ' '.repeat(4);
+
+const createSampleListText = (prefix: '-' | '*' | '>', count: number) => {
+  return [...Array(count)].map((_, i) => (i === 0 ? '' : `${PAD.repeat(i)}`) + `${prefix} hoge${i}`).join('\n');
+};
+const createSampleNumListText = (count: number) => {
+  return [...Array(count)].map((_, i) => (i === 0 ? '' : `${PAD.repeat(i)}`) + `${i}. hoge${i}`).join('\n');
+};
 
 describe('VscodeKeyboardEventクラスのテスト', () => {
   it('デフォルトで設定されたタブのサイズが 4 であることをテストする', () => {
-    // const instance = createInstance();
-    // expect(instance.TAB_SIZE).toBe(4);
+    const instance = createInstance({
+      value: '',
+      range: { start: 2, end: 2 },
+    });
+    expect(instance.TAB_SIZE).toBe(4);
   });
   describe('イベントテスト', () => {
-    it('エンター押下', () => {
+    describe('エンター押下', () => {
+      describe('カーソル選択', () => {
+        it('文字列リスト', () => {
+          const testText = createSampleListText('-', 4);
+          const instance = createInstance({
+            value: testText,
+            range: { start: 2, end: 2 },
+          });
+          instance.enter();
+        });
+        it('数値リスト', () => {
+          const testText = createSampleNumListText(4);
+          const instance = createInstance({
+            value: testText,
+            range: { start: 2, end: 2 },
+          });
+          instance.enter();
+        });
+        it('通常テキスト', () => {
+          const instance = createInstance({
+            value: NORMAL_TEXT,
+            range: { start: 2, end: 2 },
+          });
+          instance.enter();
+        });
+      });
+
+      describe('範囲選択', () => {
+        it('文字列リスト', () => {
+          const testText = createSampleListText('-', 4);
+          const instance = createInstance({
+            value: testText,
+            range: { start: 2, end: testText.length },
+          });
+          instance.enter();
+        });
+        it('数値リスト', () => {
+          const testText = createSampleNumListText(4);
+          const instance = createInstance({
+            value: testText,
+            range: { start: 2, end: testText.length },
+          });
+          instance.enter();
+        });
+        it('通常テキスト', () => {
+          const instance = createInstance({
+            value: NORMAL_TEXT,
+            range: { start: 2, end: NORMAL_TEXT.length },
+          });
+          instance.enter();
+        });
+      });
+    });
+    describe('コマンド+エンター押下', () => {
       // createInstance();
     });
-    it('コマンド+エンター押下', () => {
+    describe('タブ押下', () => {
       // createInstance();
     });
-    it('タブ押下', () => {
-      // createInstance();
-    });
-    it('シフト+タブ押下', () => {
+    describe('シフト+タブ押下', () => {
       // createInstance();
     });
   });
