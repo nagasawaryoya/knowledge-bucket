95c8ef382eb7e46b62d573ecb6cbaec8d6f57dfd
nagasawaryoya <nagasawaryoya@gmail.com>
2021-09-28 08:46:08 +0900

    型変更&削除

--- src/utils/type-util/Optional.ts
+++ src/utils/type-util/Optional.ts
@@ -1,7 +1,3 @@
-export type UndefinedOptional<T> = {
-  [P in keyof T]?: T[P];
-};
-
 export type NullOptional<T> = {
   [P in keyof T]: T[P] | null;
 };
--- src/utils/type-util/ValueOf.ts
+++ src/utils/type-util/ValueOf.ts
@@ -1,5 +1,3 @@
 export type ValueOf<T> = T[keyof T];
 
-export type DeepValueOf<T extends Readonly<T>> = T extends { [k: string | number]: T[keyof T] }
-  ? DeepValueOf<ValueOf<T>>
-  : T;
+export type DeepValueOf<T> = T extends object ? DeepValueOf<ValueOf<T>> : T;
